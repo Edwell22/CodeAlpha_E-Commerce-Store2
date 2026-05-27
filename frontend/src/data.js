df10a071ca7dd4f1660c3a1875658cb15cd6e08d
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2026-05-27 22:03:15 +0200

    new

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,22 +1,21 @@
-export default{
-    products: [
-        {
-            _id: '1',
-            name: 'Slim Shirt',
-            category: 'Shirts',
-            image: 'images/product-1.jpg',
-            price: 59,
-            brand: 'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            CountQueuingStrategy: 20,
-        },
-
-         {
+const data = {
+    "products": [
+    {
+      _id: '1',
+      name: 'Hiskywin Full Zip Running Shirts Thermal Workout',
+      category: 'Shirts',
+      image: './images/product-1.jpg',
+      price: 59,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 10,
+    },
+    {
       _id: '2',
       name: 'Nike Full-Zip Hoodie Mens Workout Sweatshirt',
       category: 'Shirts',
-      image: '/images/product-2.jpg',
+      image: './images/product-2.jpg',
       price: 70,
       brand: 'Nike',
       rating: 5,
@@ -27,7 +26,7 @@ export default{
       _id: '3',
       name: 'Adidas Mens Long Sleeve Hoodie',
       category: 'Shirts',
-      image: '/images/product-3.jpg',
+      image: './images/product-3.jpg',
       price: 59,
       brand: 'Adidas',
       rating: 4,
@@ -38,7 +37,7 @@ export default{
       _id: '4',
       name: 'Under Armour Mens Sportstyle Tricot Joggers',
       category: 'Pants',
-      image: '/images/product-4.jpg',
+      image: './images/product-4.jpg',
       price: 90,
       brand: 'Under Armour',
       rating: 2.5,
@@ -49,7 +48,7 @@ export default{
       _id: '5',
       name: 'Champion Mens Graphic Powerblend Fleece Jogger',
       category: 'Pants',
-      image: '/images/product-5.jpg',
+      image: './images/product-5.jpg',
       price: 119,
       brand: 'Champion',
       rating: 3.5,
@@ -60,13 +59,13 @@ export default{
       _id: '6',
       name: 'Aelfric Eden Mens Joggers Pants Long Multi-Pockets',
       category: 'Pants',
-      image: '/images/product-6.jpg',
+      image: './images/product-6.jpg',
       price: 85,
       brand: 'Aelfric Eden',
       rating: 4.5,
       numReviews: 45,
       countInStock: 6,
-    }, 
-    
-    ],
+    },
+  ],
 };
+export default data;
